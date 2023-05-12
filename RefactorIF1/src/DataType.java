2b1bf07600fef2a5a0ab18dce693f718e661730d
dam_mra <alemarrub4@alu.edu.gva.es>
2023-05-12 10:22:20 +0200

    3er Commit Código factorizado con array

--- RefactorIF1/src/DataType.java
+++ RefactorIF1/src/DataType.java
@@ -1,4 +1,8 @@
+
+
 public class DataType {
+    private static final int[] date = {702, 1082, 1083, 1114, 1184, 1266, 12403};
+    private static final int[] numeric = {20, 21, 23, 24, 26, 700, 701, 790, 1700, 2202, 2203, 2204, 2205, 2206, 3769, 12396};
     public static void main(String[] args) {
         System.out.println(DataType.gd(20));
         System.out.println(DataType.gd(702));
@@ -6,10 +10,18 @@ public class DataType {
     }
 
     public static String gd(int code) {
-        return switch (code) {
-            case 702, 1082, 1083, 1114, 1184, 1266, 12403 -> "DATE";
-            case 20, 21, 23, 24, 26, 700, 701, 790, 1700, 2202, 2203, 2204, 2205, 2206, 3769, 12396 -> "NUMERIC";
-            default -> "STRING";
-        };
+        for (int i : date) {
+            if (code == i) {
+                return "DATE";
+            }
+        }
+        for (int i : numeric) {
+            if (code == i) {
+                return "NUMERIC";
+            }
+        }
+        return "STRING";
     }
+
+
 }
