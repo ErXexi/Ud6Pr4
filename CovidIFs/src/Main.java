488c553b634c2981c3de7ca80551a3eb80ae18d7
dam_mra <alemarrub4@alu.edu.gva.es>
2023-05-12 10:39:45 +0200

    2do Commit Covid anidarIf

--- CovidIFs/src/Main.java
+++ CovidIFs/src/Main.java
@@ -7,33 +7,17 @@ public class Main {
                                boolean isOver60yearsold, boolean hepatic,
                                boolean kidnevDisease, boolean respiratoryDisease, String provincia
     ){
-        if (
-            (bodyTemperature>= 38 && difficultyBreathing) ||
-            (bodyTemperature>= 38 && difficultyBreathing && diabetes) ||
-            (bodyTemperature>= 38 && difficultyBreathing && cancer) ||
-            (bodyTemperature>= 38 && difficultyBreathing && isPregnant)||
-            (bodyTemperature>= 38 && difficultyBreathing && isOver60yearsold) ||
-            (bodyTemperature>= 38 && difficultyBreathing && hepatic)||
-            (bodyTemperature>= 38 && difficultyBreathing && kidnevDisease)||
-            (bodyTemperature>= 38 && difficultyBreathing && respiratoryDisease)||
-            (bodyTemperature>= 38 && diabetes)||
-            (bodyTemperature>= 38 && cancer)||
-            (bodyTemperature>= 38 && isPregnant)||
-            (bodyTemperature>= 38 && isOver60yearsold)||
-            (bodyTemperature>= 38 && hepatic)||
-            (bodyTemperature>= 38 && kidnevDisease)||
-            (bodyTemperature>= 38 && respiratoryDisease)
-        ){
-            return "/diagnostico/"+provincia;
+
+        if(bodyTemperature>=38){
+            if(difficultyBreathing){
+                if(diabetes || cancer ||isPregnant || isOver60yearsold || hepatic || kidnevDisease || respiratoryDisease){
+                    return "/diagnostico/"+provincia;
+                }
+                return "/cuarentena/";
+            }else{
+                return "/diagnostico_bueno/";
+            }
         }
-        else if(bodyTemperature>= 38){
-            return "/cuarentena/";
-        }
-        else if (bodyTemperature < 38){
-            return "/diagnostico_bueno/";
-        }
-        else{
-            return "/diagnostico_bueno/";
-        }
+        return "/diagnostico_bueno/";
     }
 }
